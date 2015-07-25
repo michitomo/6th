1ff2cb20037cfa2064f7c41aec61c3127d6b1486
vagrant <vagrant@enpit>
2015-07-25 05:06:06 +0000

    implemented sample

--- test/gem_minitest_test.rb
+++ test/gem_minitest_test.rb
@@ -1,11 +1,20 @@
 require 'test_helper'
 
-class GemMinitestTest < Minitest::Test
+class GemMinitesttest < Minitest::Test
+  def setup
+    @main = ::GemMinitest::Main.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::GemMinitest::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+#  def test_it_does_something_useful
+#    assert false
+#  end
+  def test_odd?
+    assert @main.odd?(1), '1 is odd'
+    refute @main.odd?(2), '2 is not odd'
+    assert @main.odd?(3), '3 is odd'
   end
 end
